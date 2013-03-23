96394880e7acad15977aa2de564faeb319c40637
Nezar Abdennur <nabdennur@gmail.com>
2013-03-23 16:54:51 -0400

    Created model for polls app

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,76 @@
 from django.db import models
 
-# Create your models here.
+class Instructor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    department = models.CharField(max_length=200)
+    email_address = models.EmailField()
+
+    def __unicode__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+class Course(models.Model):
+    SEMESTERS = ( 
+        ('F', 'Fall'),
+        ('J', 'IAP'),
+        ('S', 'Spring'),
+        ('E', 'Summer'),
+    )
+    code = models.CharField(max_length=30)
+    name = models.CharField(max_length=200)
+    year = models.IntegerField(default=2013)
+    semester = models.CharField(max_length=2, choices=SEMESTERS)
+    instructors = models.ManyToManyField(Instructor)
+
+    def __unicode__(self):
+        return self.name
+
+class Session(models.Model): 
+    course = models.ForeignKey(Course)
+    instructor = models.ForeignKey(Instructor)
+    date = models.DateTimeField('class date-time')
+
+    def __unicode__(self):
+        return '%s: session %s' % (course.name, self.id)
+
+class Student(models.Model):
+    LEVELS = (
+        ('U', 'Undergraduate'),
+        ('G', 'Graduate'),
+        ('S', 'Special'),
+    )
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    major = models.CharField(max_length=200)
+    level = models.CharField(max_length=2, choices=LEVELS)
+    year = models.IntegerField(default=0)
+    email_address = models.EmailField()
+    enrolled_courses = models.ManyToManyField(Course)
+    current_session = models.ForeignKey(Session)
+
+    def __unicode__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+class Quiz(models.Model):
+    TYPES = (
+        ('P', 'Poll'),
+        ('C', 'Confusometer'),
+    )
+    session = models.ForeignKey(Session)
+    qtype = models.CharField(max_length=2, choices =TYPES)
+    question = models.CharField(max_length=200)
+    choices = models.CharField(max_length=200)
+    answer = models.CharField(max_length=200)
+    post_time = models.TimeField()
+
+    def __unicode__(self):
+        return self.question
+
+class Response(models.Model):
+    student = models.ForeignKey(Student)
+    quiz = models.ForeignKey(Quiz)
+    value = models.CharField(max_length=200)
+    time = models.TimeField()
+
+    def __unicode__(self):
+        return 'Time:%s, Ans:%s' % (self.time, self.value)
--- polls/tests.py
+++ polls/tests.py
@@ -8,9 +8,105 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from django.utils import timezone
+from datetime import timedelta
+from polls.models import *
+
+class TestDB(TestCase):
+    def setUp(self):
+        # Instructors
+        self.inst = Instructor(first_name='David',
+                               last_name='Gifford',
+                               department='EECS',
+                               email_address='fake@mit.edu')
+        self.inst.save()
+
+        
+        # Courses
+        self.course = Course(code='7.91', 
+                             name='Intro to Comp & Sys Bio',
+                             year=2013,
+                             semester='S')        
+        self.course.save()
+        self.course.instructors.add(self.inst)
+
+        self.course2 = Course(code='6.437',
+                             name='Inference and Info',
+                             year=2013,
+                             semester='S')
+        self.course2.save()
+        self.course2.instructors.add(self.inst)
+
+        
+        # Sessions
+        self.session = Session(course=self.course, 
+                               instructor=self.inst,
+                               date=timezone.now())
+        self.session.save()
+        
+
+        # Students
+        self.stud = Student(first_name='Vincent',
+                            last_name='Xue',
+                            major='CSB',
+                            level='G',
+                            year='1',
+                            email_address='vxue@gmail.com', 
+                            current_session=self.session)
+        self.stud.save()
+        self.stud.enrolled_courses.add(self.course)
+
+        self.stud2 = Student(first_name='Mariana',
+                            last_name='Matus',
+                            major='CSB',
+                            level='G',
+                            year='1',
+                            email_address='mgmatus@mit.edu', 
+                            current_session=self.session)
+        self.stud2.save()
+        self.stud2.enrolled_courses.add(self.course)
+        self.stud2.enrolled_courses.add(self.course2)
+
+
+        # Quizzes
+        self.quiz = Quiz(session=self.session,
+                         qtype='P',
+                         question='What time is it?', 
+                         choices='1|2|3|4',
+                         answer='1',
+                         post_time=timezone.now())
+        self.quiz.save()
+        
+
+        # Responses
+        self.resp = Response(student=self.stud,
+                             quiz=self.quiz,
+                             value='2',
+                             time=self.quiz.post_time+timedelta(seconds=20))
+        self.resp2 = Response(student=self.stud2,
+                              quiz=self.quiz,
+                              value='1',
+                              time=self.quiz.post_time+timedelta(seconds=30))
+        self.resp.save()
+        self.resp2.save()
+        
+
+    def test_Students(self):
+        s = Student.objects.filter(major='CSB')
+        self.assertEqual(s[0].first_name, 'Vincent')
+        self.assertEqual(s[1].first_name, 'Mariana')
+
+    def test_QuizResponses(self):
+        q = Quiz.objects.filter(qtype='P')
+        self.assertEqual(q[0].question,'What time is it?')
+
+        r = Response.objects.filter(student__first_name='Vincent')
+        r_vincent = r[0]
+        self.assertEqual(r_vincent.value,'2')
+
+        q = Quiz.objects.get(id=1)
+        r = Response.objects.filter(quiz=q, value=q.answer)
+        r_mariana = r[0]
+        self.assertEqual(r_mariana.student.first_name, 'Mariana')
+
+        self.assertGreater(r_mariana.time, r_vincent.time)

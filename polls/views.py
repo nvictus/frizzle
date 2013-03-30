b155cfaaa708c4a673e6ecfa4de9c6867c999efe
Nezar Abdennur <nabdennur@gmail.com>
2013-03-30 01:55:19 -0400

    added view method to display http request META for debugging

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
+
+def display_meta(request):
+    values = request.META.items()
+    values.sort()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
